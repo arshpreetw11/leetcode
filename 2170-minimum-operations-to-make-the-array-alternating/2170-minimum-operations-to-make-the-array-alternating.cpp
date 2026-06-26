9e985691ce5c121e4daf1842f04bec224577a87c
Arshpreet Walia <arshpreetw11@gmail.com>
2026-06-26 12:51:09 +0530

    Time: 58 ms (93.83%), Space: 108 MB (96.3%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -1,36 +1,16 @@
 class Solution {
+    int freq[100005][2];
 public:
     int minimumOperations(vector<int>& nums) {
         int n =nums.size();
-        unordered_map<int,int> freqODD,freqEVEN;
-        for(int i=0;i<n;i+=2){
-            freqEVEN[nums[i]]++;
-            if(i+1<n){
-                freqODD[nums[i+1]]++;
-            }
+        memset(freq,0,sizeof(freq));
+        int i,j,k,ans=0;
+        for(i=0;i<n;i++) freq[nums[i]][i&1]++;
+        for(i=1, j=k=0;i<=100000;i++){
+            ans=max(ans,max(freq[i][0]+k,freq[i][1]+j));
+            j=max(j,freq[i][0]);
+            k=max(k,freq[i][1]);
         }
-        vector<pair<int,int>> odd,even;
-        for(auto &e : freqEVEN){
-            even.push_back({e.second,e.first});
-        }
-
-        for(auto &o : freqODD){
-            odd.push_back({o.second,o.first});}
-
-        even.push_back({0,-1});
-        even.push_back({0,-2});
-        odd.push_back({0,-1});
-        odd.push_back({0,-2});
-
-        sort(even.rbegin(),even.rend());
-        sort(odd.rbegin(),odd.rend());
-
-        if(even[0].second!=odd[0].second){
-            return n-even[0].first-odd[0].first;
-        }
-
-        return min(n-even[1].first-odd[0].first,
-                   n-even[0].first-odd[1].first);
-        
+        return n-ans;
     }
 };
