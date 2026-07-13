8500c031ad367f59e73ee1ba5e6542ab52f5a2cc
Arshpreet Walia <arshpreetw11@gmail.com>
2026-07-13 11:56:52 +0530

    Time: 26 ms (47.71%), Space: 20.9 MB (79.47%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -11,24 +11,22 @@ public:
 class Solution {
 public:
     int total=0;
-    void dfs(int id,unordered_map<int,pair<int,vector<int>>>& adj){
-        total+=adj[id].first;
-        if(adj[id].second.size()==0) return;
+    void dfs(int id,unordered_map<int,Employee*>& adj){
+        total+=adj[id]->importance;
+        if(adj[id]->subordinates.size()==0) return;
         
-        for(int sub:adj[id].second){
+        for(int sub:adj[id]->subordinates){
             dfs(sub,adj);
         }
     }
     int getImportance(vector<Employee*> employees, int id) {
         int n=employees.size(); //number of employees;
         
-        unordered_map<int,pair<int,vector<int>>> adj;
+        unordered_map<int,Employee*> adj;
         for(auto &emp:employees){
+            
             int ID=emp->id;
-            int imp=emp->importance;
-            vector<int> subor=emp->subordinates;
-            adj[ID].first=imp;
-            adj[ID].second=subor;
+            adj[ID]=emp;
         }
         dfs(id,adj);
         return total;
