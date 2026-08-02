4d9506bf07763e67c1cbfc9b98308539d947f881
Arshpreet Walia <arshpreetw11@gmail.com>
2026-08-02 11:41:23 +0530

    Time: 7 ms (35.23%), Space: 10.6 MB (46.05%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-vector<vector<int>> dp;
-    int dfs(int start,int end,vector<int>& piles){
+// vector<vector<int>> dp;
+//     int dfs(int start,int end,vector<int>& piles){
 
-        if(start<0 || end>=piles.size()) return 0;
-        if(dp[start][end]!=-1) return dp[start][end];
-        if(start==end){
-            return dp[start][end]=piles[start];
-        }
-        int takeLeft=piles[start]-dfs(start+1,end,piles);
-        int takeRight=piles[end]-dfs(start,end-1,piles);
-        return dp[start][end]=max(takeLeft,takeRight);
-    }
+//         if(start<0 || end>=piles.size()) return 0;
+//         if(dp[start][end]!=-1) return dp[start][end];
+//         if(start==end){
+//             return dp[start][end]=piles[start];
+//         }
+//         int takeLeft=piles[start]-dfs(start+1,end,piles);
+//         int takeRight=piles[end]-dfs(start,end-1,piles);
+//         return dp[start][end]=max(takeLeft,takeRight);
+//     }
     bool stoneGame(vector<int>& piles) {
-        dp.assign(piles.size(),vector<int>(piles.size(),-1));
-        return dfs(0,piles.size()-1,piles)>0;
+        int n=piles.size();
+        vector<int> dp=piles;
+        for(int len=2;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                dp[i]=max(piles[i]-dp[i+1],
+                        piles[j]-dp[i]);
+            }
+        }
+        return dp[0]>0;
+        
     }
 };
