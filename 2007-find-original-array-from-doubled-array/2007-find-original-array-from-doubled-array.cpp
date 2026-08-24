d10de64ed754fe64f46d411325ec4a2fca29b331
Arshpreet Walia <arshpreetw11@gmail.com>
2026-08-24 15:59:29 +0530

    Time: 144 ms (48.16%), Space: 145.7 MB (43.96%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -4,40 +4,21 @@ public:
         int dbl=changed.size();
         if(dbl%2) return {};
         int n=dbl/2;
-
-        int find=0;
         unordered_map<int,int> freq;
         for(int x:changed){
             freq[x]++;
         }
         vector<int> ans;
-        unordered_set<int> st(changed.begin(),changed.end());
         sort(changed.begin(),changed.end());
-        for(int i=0;i<dbl;i++){
-            int num=changed[i];
-            int dnum=2*num;
-            if(st.count(num)){
-                if(num==dnum){
-                    if(freq[num]>1){
-                        freq[num]-=2;
-                        ans.push_back(num);
-                        if(freq[num]==0)
-                        st.erase(num);
-                    }
-                }
-                else{
-                    if(st.count(dnum)){
-                        freq[dnum]--;
-                        freq[num]--;
-                        ans.push_back(num);
-                        if(freq[dnum]==0) st.erase(dnum);
-                        if(freq[num]==0) st.erase(num);
-
-                    }
-                }
+        for(int x:changed){
+            if(freq[x]==0) continue;
+            if(freq[2*x]==0){
+                return {};
             }
+            ans.push_back(x);
+            freq[x]--;
+            freq[2*x]--;
         }
-        if(ans.size()==n) return ans;
-        return {};
+        return ans;
     }
 };
